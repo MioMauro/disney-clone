a2732a36e7a705290e008c50aa105b5d4907f1a5
Mauro.V <remanomauro@gmail.com>
2022-10-22 10:51:49 +0200

    set up fixed image bacground

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,28 @@ import styled from 'styled-components'
 
 function Home() {
   return (
-    <Container>Home</Container>
+    <Container>
+    Home
+    </Container>
   )
 }
 
 export default Home
 
-const Container = styled.div`
-background: green;
+const Container = styled.main`
+color: white;
+min-height: calc(100vh - 70px);
+padding: 0 calc(3.5vw + 5px);
+position: relative;
+
+&:before {  
+  background: url("/images/home-background.png") center center / cover no-repeat fixed;
+  content: "";
+  position: absolute;  
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+}
 `
